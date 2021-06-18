85f74e6144f43a8c9d3779d31804fabdd8fc935d
Eason <easonlai@msn.com>
2021-06-18 12:24:28 +0800

    fix on server ip logging

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var winston = require('winston');
 var app = express();
 var fs = require("fs");
+var ip = require("ip");
 
 const logConfiguration = {
   'transports': [
@@ -30,27 +31,27 @@ app.get('/listSAs', (req, res) => {
    fs.readFile( __dirname + '/' + '/data/users.json', 'utf8', function (err, data) {
        //console.log( "/listUsers API is being GET." );
        res.statusMessage = 'OK';
-       logger.info(`200, ${res.statusMessage}, ${req.originalUrl}, ${req.method}, ${req.ip}, ${server.address().address}`);
+       logger.info(`200, ${res.statusMessage}, ${req.originalUrl}, ${req.method}, ${req.ip}, ${ip.address()}`);
        res.end( data );
    });
 })
 
 app.use((req,res,next) => {
   res.status(404).send('400 PAGE NOT FOUND');
-  logger.error(`400, ${res.statusMessage}, ${req.originalUrl}, ${req.method}, ${req.ip}, ${server.address().address}`);
+  logger.error(`400, ${res.statusMessage}, ${req.originalUrl}, ${req.method}, ${req.ip}, ${ip.address()}`);
 })
 
 // Capture 500 errors
 app.use((err,req,res,next) => {
   res.status(500).send('500 SERVER ERROR');
-     logger.error(`${err.status || 500}, ${err.message}, ${req.originalUrl}, ${req.method}, ${req.ip}, ${server.address().address}`);
+     logger.error(`${err.status || 500}, ${err.message}, ${req.originalUrl}, ${req.method}, ${req.ip}, ${ip.address()}`);
   })
 
 var server = app.listen(8081, function () {
 
   var host = server.address().address
   var port = server.address().port
-
+  //console.log(ip.address())
   //console.log("RestAPI is listening at http://%s:%s", host, port)
 
 })
